01e8e103bda2e91ad55df2a6960077902f691a1b
Berenice Barcenas Mancilla <berenice.barcenas.mancilla@gmail.com>
2024-01-26 17:20:08 -0600

    Correccion calculo de consultorio

--- Control_consultorio/public/js/admin/pacientes.js
+++ Control_consultorio/public/js/admin/pacientes.js
@@ -87,55 +87,55 @@ var KTDatatableAutoColumnHideDemo = function () {
 
                     }
                 },
-                {
-                    field: 'Actions',
-                    title: 'Acciones',
-                    sortable: false,
-                    width: 125,
-                    overflow: 'visible',
-                    autoHide: false,
-                    template: function (row) {
-                        var content = '';
+                // {
+                //     field: 'Actions',
+                //     title: 'Acciones',
+                //     sortable: false,
+                //     width: 125,
+                //     overflow: 'visible',
+                //     autoHide: false,
+                //     template: function (row) {
+                //         var content = '';
                 
-                        content += '<li class="navi-item">\
-                            <div class="dropdown dropdown-inline">\
-                                <a href="javascript:;" class="btn btn-sm btn-clean btn-icon mr-2" data-toggle="dropdown">\
-                                    <span class="svg-icon svg-icon-md">\
-                                        <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="24px" height="24px" viewBox="0 0 24 24" version="1.1">\
-                                            <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">\
-                                                <rect x="0" y="0" width="24" height="24"/>\
-                                                <path d="M5,8.6862915 L5,5 L8.6862915,5 L11.5857864,2.10050506 L14.4852814,5 L19,5 L19,9.51471863 L21.4852814,12 L19,14.4852814 L19,19 L14.4852814,19 L11.5857864,21.8994949 L8.6862915,19 L5,19 L5,15.3137085 L1.6862915,12 L5,8.6862915 Z M12,15 C13.6568542,15 15,13.6568542 15,12 C15,10.3431458 13.6568542,9 12,9 C10.3431458,9 9,10.3431458 9,12 C9,13.6568542 10.3431458,15 12,15 Z" fill="#000000"/>\
-                                            </g>\
-                                        </svg>\
-                                    </span>\
-                                </a>\
-                                <div class="dropdown-menu dropdown-menu-sm dropdown-menu-right">\
-                                    <ul class="navi flex-column navi-hover py-2">\
-                                        <li class="navi-header font-weight-bolder text-uppercase font-size-xs text-dark pb-2">\
-                                            Selecciona una opción:\
-                                        </li>\
-                                        <li>\
-                                            <a href="/sintomas" class="btn btn-light font-weight-bolder" data-toggle="modal" data-target="#sintomaNewModal">\
-                                                <span class="svg-icon svg-icon-md">\
-                                                    <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="24px" height="24px" viewBox="0 0 24 24" version="1.1">\
-                                                        <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">\
-                                                            <rect x="0" y="0" width="24" height="24" />\
-                                                            <circle fill="#000000" cx="9" cy="15" r="6" />\
-                                                            <path d="M8.8012943,7.00241953 C9.83837775,5.20768121 11.7781543,4 14,4 C17.3137085,4 20,6.6862915 20,10 C20,12.2218457 18.7923188,14.1616223 16.9975805,15.1987057 C16.9991904,15.1326658 17,15.0664274 17,15 C17,10.581722 13.418278,7 9,7 C8.93357256,7 8.86733422,7.00080962 8.8012943,7.00241953 Z" fill="#000000" opacity="0.3" />\
-                                                        </g>\
-                                                    </svg>\
-                                                </span>\
-                                                Nuevo síntoma\
-                                            </a>\
-                                        </li>\
-                                    </ul>\
-                                </div>\
-                            </div>\
-                        </li>';
+                //         content += '<li class="navi-item">\
+                //             <div class="dropdown dropdown-inline">\
+                //                 <a href="javascript:;" class="btn btn-sm btn-clean btn-icon mr-2" data-toggle="dropdown">\
+                //                     <span class="svg-icon svg-icon-md">\
+                //                         <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="24px" height="24px" viewBox="0 0 24 24" version="1.1">\
+                //                             <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">\
+                //                                 <rect x="0" y="0" width="24" height="24"/>\
+                //                                 <path d="M5,8.6862915 L5,5 L8.6862915,5 L11.5857864,2.10050506 L14.4852814,5 L19,5 L19,9.51471863 L21.4852814,12 L19,14.4852814 L19,19 L14.4852814,19 L11.5857864,21.8994949 L8.6862915,19 L5,19 L5,15.3137085 L1.6862915,12 L5,8.6862915 Z M12,15 C13.6568542,15 15,13.6568542 15,12 C15,10.3431458 13.6568542,9 12,9 C10.3431458,9 9,10.3431458 9,12 C9,13.6568542 10.3431458,15 12,15 Z" fill="#000000"/>\
+                //                             </g>\
+                //                         </svg>\
+                //                     </span>\
+                //                 </a>\
+                //                 <div class="dropdown-menu dropdown-menu-sm dropdown-menu-right">\
+                //                     <ul class="navi flex-column navi-hover py-2">\
+                //                         <li class="navi-header font-weight-bolder text-uppercase font-size-xs text-dark pb-2">\
+                //                             Selecciona una opción:\
+                //                         </li>\
+                //                         <li>\
+                //                             <a href="/sintomas" class="btn btn-light font-weight-bolder" data-toggle="modal" data-target="#sintomaNewModal">\
+                //                                 <span class="svg-icon svg-icon-md">\
+                //                                     <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="24px" height="24px" viewBox="0 0 24 24" version="1.1">\
+                //                                         <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">\
+                //                                             <rect x="0" y="0" width="24" height="24" />\
+                //                                             <circle fill="#000000" cx="9" cy="15" r="6" />\
+                //                                             <path d="M8.8012943,7.00241953 C9.83837775,5.20768121 11.7781543,4 14,4 C17.3137085,4 20,6.6862915 20,10 C20,12.2218457 18.7923188,14.1616223 16.9975805,15.1987057 C16.9991904,15.1326658 17,15.0664274 17,15 C17,10.581722 13.418278,7 9,7 C8.93357256,7 8.86733422,7.00080962 8.8012943,7.00241953 Z" fill="#000000" opacity="0.3" />\
+                //                                         </g>\
+                //                                     </svg>\
+                //                                 </span>\
+                //                                 Nuevo síntoma\
+                //                             </a>\
+                //                         </li>\
+                //                     </ul>\
+                //                 </div>\
+                //             </div>\
+                //         </li>';
                 
-                        return content;
-                    },
-                }
+                //         return content;
+                //     },
+                // }
                 
             ],
 
